c1006d4cf4151fd474cfac00c152291398286b52
J Alan Brogan <github@al-got-rhythm.net>
2014-05-13 09:29:27 +0100

    script to provide commands to bash

--- jpm/__init__.py
+++ jpm/__init__.py
@@ -2,5 +2,3 @@
 
 __version__ = '0.0.2'
 
-
-from .jpm import play
--- setup.py
+++ setup.py
@@ -37,5 +37,6 @@ setup(
         'docs': ['Sphinx'],
         'development': ['pudb'],
         'testing': ['nose'],
-    }
+    },
+    keywords=["mpd"],
 )
