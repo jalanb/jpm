5033cf506175b01e22144a301fb0c64a2ef00b17
J Alan Brogan <github@al-got-rhythm.net>
2021-01-12 01:40:29 +0000

    Use pysyte to run the program

--- jpm/cli/jpm.py
+++ jpm/cli/jpm.py
@@ -2,15 +2,12 @@ import os
 import sys
 
 
-def play(args):
-    pass
+from pysyte.cli.main import run
 
 
 def main(args):
     """Run the program"""
-    try:
-        play(args)
-    except Exception as e:
-        print(e, file=sys.stderr)
-        return not os.EX_OK
-    return os.EX_OK
+    return True
+
+
+run(main)
